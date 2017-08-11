732a37f980523fd03d9b56bd47ffcc0b0e5d2801
dandanloveJM <gigi834921748@qq.com>
2017-08-11 01:53:14 -0700

    改写getUsers方法

--- models/mongo/user.js
+++ models/mongo/user.js
@@ -13,7 +13,18 @@ async function createANewUser(params) {
     await user.save()
         .then()
         .catch(e => {
-            
+
         })
     return user
+}
+//1.出于安全性的考虑，一次只给一部分用户数据；2.给服务器减小压力
+async function getUsers(params = { page: 0, pageSize: 10 }) {
+    let flow = UserModel.find({})
+    flow.skip(params.page * params.pageSize)
+    flow.limit(params.pageSize)
+    return await flow
+    .catch(e=>{
+        console.log(e)
+        throw new Error('erroe getting users from db')
+    })
 }
