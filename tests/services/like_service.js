6574dd047a335026d05dbce64219caa3613a8003
dandanloveJM <gigi834921748@qq.com>
2017-09-24 10:06:32 -0700

    fix little bug

--- tests/services/like_service.js
+++ tests/services/like_service.js
@@ -1,9 +1,9 @@
-require('../../models/mongo/user')
-require('../../mocks/user')
-require.cache['/home/mcavoy/myexpress/myapp/models/mongo/user.js'] =
-    require.cache['/home/mcavoy/myexpress/myapp/mocks/user.js']
 
-const User = require('../../mocks/user')
+// require('../../mocks/user')
+// require.cache['/home/mcavoy/myexpress/myapp/models/mongo/user.js'] =
+//     require.cache['/home/mcavoy/myexpress/myapp/mocks/user.js']
+
+let User = require('../../models/mongo/user')
 const LikeService = require('../../services/like_service')
 const { expect } = require('chai')
 const sinon = require('sinon')
@@ -14,17 +14,21 @@ describe('likeService#likeTopic', async () => {
 
     //首先看用户有没有点过赞
     //加分
-    //测试开始前设为false
+    //测试开始前
     beforeEach(async () => {
-        User.incrPointsCalled = false
+        User = sinon.mock(User)
+        
     })
     //测试结束后重新设为初始状态
     afterEach(async () => {
-        User.incrPointsCalled = false
+        
     })
 
     it('should called User.incrPoints with an valid objectId', async () => {
+        User.expect('incrPoints').once()
         await LikeService.likeTopic(userId, 'attachedId')
+        User.verify()
+        
     })
     //当userid不合法时就抛出一个错误
     it('should throw an error when invalid userId passed', async () => {
