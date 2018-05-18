187fc696ed500d1e42155ddd40dafaeadbb1a281
dandanloveJM <gigi834921748@qq.com>
2018-05-18 10:07:56 -0700

    send msg to user apidoc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-18T17:01:22.244Z",
+    "time": "2018-05-18T17:07:25.119Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
