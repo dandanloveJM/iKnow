1b17a6f0615881e08633b16e07eb733286440d9a
dandanloveJM <gigi834921748@qq.com>
2018-05-21 09:10:39 -0700

    fix merge wrong

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "iKnow",
+  "version": "0.1.0",
+  "description": "A QA community for college students",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-05-18T17:07:25.119Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
