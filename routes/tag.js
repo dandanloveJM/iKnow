99a8bedf8259c2e05266e41ff2a0126096488e87
dandanloveJM <gigi834921748@qq.com>
2018-05-24 02:14:27 -0700

    add auth in tags

--- routes/tag.js
+++ routes/tag.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Tag = require('../models/mongo/tag')
 const TagService = require('../services/jieba_service')
+const auth = require('../middlewares/auth_user')
 
 
 /* localhost:8082/tag/ */
